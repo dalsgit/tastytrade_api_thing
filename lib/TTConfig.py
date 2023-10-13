0a2f3965a0b338bb2e3bbd99325bf4476ac2fb0e
dalsgit <daljeetsm@gmail.com>
2023-10-13 14:15:13 -0400

    Update TTConfig.py

--- lib/TTConfig.py
+++ lib/TTConfig.py
@@ -1,18 +1,18 @@
 import configparser
-
-
 class TTConfig:
     config: configparser.ConfigParser = configparser.ConfigParser()
     use_prod: bool = False
     use_mfa: bool = False
     username: str = None
     password: str = None
+    cert_username: str = None
+    cert_password: str = None
     cert_uri: str = None
     prod_uri: str = None
     cert_wss: str = None
     prod_wss: str = None
 
-    def __init__(self, path: str = "./config", filename: str = "tt.config") -> None:
+    def __init__(self, path: str = "./", filename: str = "tt.config") -> None:
         self.config.read(f"{path}/{filename}")
         self.use_prod = self.config.get("Config", "use_prod") in (
             "True",
@@ -34,7 +34,10 @@ class TTConfig:
         )
         self.username = self.config.get("Credentials", "username")
         self.password = self.config.get("Credentials", "password")
+        self.cert_username = self.config.get("Credentials", "cert.username")
+        self.cert_password = self.config.get("Credentials", "cert.password") 
         self.cert_uri = self.config.get("URI", "cert")
         self.prod_uri = self.config.get("URI", "prod")
         self.cert_wss = self.config.get("WSS", "cert")
         self.prod_wss = self.config.get("WSS", "prod")
+
